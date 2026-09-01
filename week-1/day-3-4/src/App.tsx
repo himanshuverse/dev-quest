b9cfc1cc959fc1824232833362b348f158b5da9f
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-09-01 15:07:54 +0530

    day 4 task - toggle btn implement

--- week-1/day-3-4/src/App.tsx
+++ week-1/day-3-4/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 
+
+type Todo={
+  text:String,
+  completed:boolean
+}
+
 function App() {
-const [todos,setTodos]= useState<string[]>([])
+const [todos,setTodos]= useState<Todo[]>([])
 const [task,setTask]=useState("")
 
 function addTask(){
-  setTodos([...todos,task])
+  setTodos([...todos,{text:task,completed:false}])
   setTask("")
 }
 
@@ -14,20 +20,46 @@ function deleteTodo(toDeleteIndex:number){
   setTodos(todos.filter((todo, index)=>index!==toDeleteIndex))
 }
 
+function toggleTask(indexToToggle: number) {
+  setTodos(
+    todos.map((todo, index) => {
+      if (index === indexToToggle) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      }
+
+      return todo
+    })
+  )
+}
+
   return (
     <div>
       <div style={{display:'flex',gap:'10px'}}>
-        <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} placeholder='enter task' />
+        <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} placeholder='enter task' onKeyDown={(e)=>{
+          if(e.key=='Enter') return addTask()
+        }} />
         <button onClick={addTask}>submit</button>
       </div>
       <div>
          {todos.map((todo, index) => {
   return (
     <div key={index}>
-      <p>{todo}</p>
+       <span
+                style={{
+                  textDecoration: todo.completed
+                    ? 'line-through'
+                    : 'none'
+                }}
+              >
+                {todo.text}
+              </span>
       <button onClick={() => deleteTodo(index)}>
         Delete
       </button>
+      <button onClick={()=>toggleTask(index)}>  {todo.completed ? 'Undo' : 'Complete'}</button>
     </div>
   )
 })}
